e205657dbd5d3bfb762ca3f9b5c04be59721443b
Demijan-HomePC <36194841+demijan-ha@users.noreply.github.com>
2020-04-17 12:58:25 +0200

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:nightlife/theme/style.dart';
-import 'package:nightlife/routes.dart';
-import 'package:nightlife/bloc/bloc-prov-tree.dart';
-import 'package:nightlife/bloc/bloc-prov.dart';
-import 'package:nightlife/blocs/blocs.dart';
-import 'blocs/blocs.dart';
 
 void main() {
-  runApp(ExampleApp());
+  runApp(MaterialApp());
 }
 
-class ExampleApp extends StatelessWidget {
+class MaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProviderTree(
-      blocProviders: <BlocProvider>[
-        BlocProvider<AuthBloc>(bloc: AuthBloc()),
-        BlocProvider<PrefBloc>(bloc: PrefBloc()),
-      ],
-      child: MaterialApp(
-        title: 'ExampleApp',
-        theme: appTheme(),
-        initialRoute: '/',
-        routes: routes,
-      ),
-    );
+    return Container();
   }
 }
